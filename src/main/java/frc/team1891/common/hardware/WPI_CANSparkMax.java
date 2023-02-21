e8180dd096d0bbcb26a01bd1c620359a9f0fe7ac
StephenDay1 <57306301+StephenDay1@users.noreply.github.com>
2023-02-21 15:45:42 -0400

    Update to WPILib 2023.4.1
    Also add WPI_CANSparkMax wrapper for CANSparkMax

---
+++ src/main/java/frc/team1891/common/hardware/WPI_CANSparkMax.java
@@ -0,0 +1,30 @@
+package frc.team1891.common.hardware;
+
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.util.sendable.SendableRegistry;
+
+public class WPI_CANSparkMax extends CANSparkMax implements Sendable {
+    /**
+     * Create a new object to control a SPARK MAX motor Controller
+     *
+     * @param deviceId The device ID.
+     * @param type     The motor type connected to the controller. Brushless motor wires must be connected
+     *                 to their matching colors and the hall sensor must be plugged in. Brushed motors must be
+     *                 connected to the Red and Black terminals only.
+     */
+    public WPI_CANSparkMax(int deviceId, MotorType type) {
+        super(deviceId, type);
+
+        SendableRegistry.addLW(this, "SparkMax ", deviceId);
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.setSmartDashboardType("Motor Controller");
+        builder.setActuator(true);
+        builder.setSafeState(this::stopMotor);
+        builder.addDoubleProperty("Value", this::get, this::set);
+    }
+}
